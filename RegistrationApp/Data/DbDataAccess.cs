b9e1a60ccbe7425af3f71308403efa7b44dff97f
алина человечкова <alinasmart19@mail.ru>
2021-03-03 19:26:44 +0600

    Добавьте файлы проекта.

---
+++ RegistrationApp/Data/DbDataAccess.cs
@@ -0,0 +1,30 @@
+﻿using RegistrationApp.Service;
+using System;
+using System.Collections.Generic;
+using System.Data.Common;
+
+namespace RegistrationApp.Data
+{
+    public abstract class DbDataAccess<T> : IDisposable
+    {
+        protected readonly DbProviderFactory factory;
+        protected readonly DbConnection connection;
+
+
+        public DbDataAccess()
+        {
+            factory = DbProviderFactories.GetFactory("RegistrationAppProvider");
+
+            connection = factory.CreateConnection();
+            connection.ConnectionString = ConfigurationService.Configuration["dataAccessConnectionString"];
+            connection.Open();
+        }
+
+        public void Dispose()
+        {
+            connection.Close();
+        }
+
+        public abstract void Insert(T entity);
+    }
+}
---
+++ RegistrationApp/Service/ConfigurationService.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.Data.Common;
+using System.Data.SqlClient;
+namespace RegistrationApp.Service
+{
+    public class ConfigurationService
+    {
+        public static IConfiguration Configuration { get; private set; }
+
+        public static void Init()
+        {
+            if (DbProviderFactories.TryGetFactory("RegistrationAppProvider", out DbProviderFactory factory) == false)
+            {
+                DbProviderFactories.RegisterFactory("RegistrationAppProvider", SqlClientFactory.Instance);
+            }
+
+            if (Configuration == null)
+            {
+                var configurationBuilder = new ConfigurationBuilder();
+                Configuration = configurationBuilder.AddJsonFile("appSettings.json").Build();
+
+            }
+        }
+    }
+}
